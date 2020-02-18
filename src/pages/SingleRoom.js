d87add3c57ad0f11d5d6ad4eb5d62fe6316a4669
Benjamin Aragon <baragon23@gmail.com>
2020-02-18 12:59:07 -0800

    first commit of work in progress

---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import defaultBcg from '../images/room-1.jpeg';
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import { Link } from 'react-router-dom';
+import { RoomContext } from '../context';
+
+export default class SingleRoom extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			slug: this.props.match.params.slug,
+			defaultBcg: defaultBcg
+		};
+	}
+
+	static contextType = RoomContext;
+
+	render() {
+		const { getRoom } = this.context;
+		const room = getRoom(this.state.slug);
+		console.log(room);
+
+		return (
+			<div>
+				Hello from single room page
+			</div>
+		)
+	}
+}
